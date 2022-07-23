815f8746d79ec53ba2076f3ed947d143bfb21120
Tay Kai Quan <kaitay303@gmail.com>
2022-07-23 11:08:11 +0800

    Added fancy emoji keyboard

--- Bouncy Emojis App/EmojiSetCreateView.swift
+++ Bouncy Emojis App/EmojiSetCreateView.swift
@@ -19,9 +19,7 @@ struct EmojiSetCreateView: View {
         Form {
             Section("Emoji Set Details") {
                 TextField("Set Name", text: $name)
-                    .textFieldStyle(.roundedBorder)
-                TextField("Emojis (split by comma)", text: $emojis)
-                    .textFieldStyle(.roundedBorder)
+                EmojiTextField(text: $emojis, placeholder: "Emoji Set Emojis")
                 ColorPicker("Background color", selection: $bgColor)
             }
             Section {
@@ -39,6 +37,67 @@ struct EmojiSetCreateView: View {
             }
         }
         .navigationTitle("New Emoji Set")
+    }
+}
+
+// copy pasted code
+class UIEmojiTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    func setEmoji() {
+        _ = self.textInputMode
+    }
+
+    override var textInputContextIdentifier: String? {
+           return ""
+    }
+
+    override var textInputMode: UITextInputMode? {
+        for mode in UITextInputMode.activeInputModes {
+            if mode.primaryLanguage == "emoji" {
+                self.keyboardType = .default // do not remove this
+                return mode
+            }
+        }
+        return nil
+    }
+}
+
+struct EmojiTextField: UIViewRepresentable {
+    @Binding var text: String
+    var placeholder: String = ""
+
+    func makeUIView(context: Context) -> UIEmojiTextField {
+        let emojiTextField = UIEmojiTextField()
+        emojiTextField.placeholder = placeholder
+        emojiTextField.text = text
+        emojiTextField.delegate = context.coordinator
+        return emojiTextField
+    }
+
+    func updateUIView(_ uiView: UIEmojiTextField, context: Context) {
+        uiView.text = text
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+
+    class Coordinator: NSObject, UITextFieldDelegate {
+        var parent: EmojiTextField
+
+        init(parent: EmojiTextField) {
+            self.parent = parent
+        }
+
+        func textFieldDidChangeSelection(_ textField: UITextField) {
+            DispatchQueue.main.async { [weak self] in
+                self?.parent.text = textField.text ?? ""
+            }
+        }
     }
 }
 
