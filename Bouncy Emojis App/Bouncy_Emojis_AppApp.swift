1b92c8cd7dba1e66066fdabaa68224e9da9cb165
Small kid <alpheuslaw2008@gmail.com>
2022-07-23 09:46:37 +0800

    Initial Commit

---
+++ Bouncy Emojis App/Bouncy_Emojis_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bouncy_Emojis_AppApp.swift
+//  Bouncy Emojis App
+//
+//  Created by swift_user on 23/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Bouncy_Emojis_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bouncy Emojis App/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Bouncy Emojis App
+//
+//  Created by swift_user on 23/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
